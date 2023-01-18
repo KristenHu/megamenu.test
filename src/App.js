f70523a63f88b8be8aa9e6bc3a34fcfd89d01b99
Kristen <kristenhu@ssw.com.au>
2023-01-18 14:46:45 +0800

    ✔Add megaMenu component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Menu, MobileMenu } from 'ssw.megamenu';
 
-function App() {
+const App = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const actionOnToggleClick = () => {
+    setIsMenuOpened(!isMenuOpened);
+  };
+
+
   return (
     <div className="App">
+      <Menu onClickToggle={() => actionOnToggleClick()}></Menu>
+      <MobileMenu isMenuOpened={isMenuOpened}></MobileMenu>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
